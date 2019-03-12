fb9dd80d3fec3d809892523d66c72de3e537c629
Russ <RussC243@optonline.net>
2019-03-12 07:12:45 -0400

    added confiuration for drive speed controllers for our 3 bots

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -6,9 +6,9 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
-//import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
-import edu.wpi.first.wpilibj.VictorSP;
+//import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Robot.OurBots;
@@ -19,58 +19,67 @@ import edu.wpi.first.wpilibj.*;
 */
 public class DriveTrain 
 {
-  //peanut bot uses spark speed controlers
-  Spark           left_front_2nd_and_peanut = new Spark(0);	
-  Spark           left_rear_peanut 	= new Spark(1);
-  Spark           right_front_peanut= new Spark(2);
-  Spark           right_rear_peanut	= new Spark(3);
-  HardwareMap hMap = new HardwareMap();
-
-  // bag bot uses victor speed controlers on can bus
-  WPI_VictorSPX   left_front_bag	= new WPI_VictorSPX(hMap.driveMotorLeftFront);	
-  WPI_VictorSPX   left_rear_bag 	= new WPI_VictorSPX(hMap.driveMotorLeftRear);
-  WPI_VictorSPX   right_front_bag	= new WPI_VictorSPX(hMap.driveMotorRightFront);
-  WPI_VictorSPX   right_rear_bag	= new WPI_VictorSPX(hMap.driveMotorRightRear);
+  //todo: move magic numbers to HardwareMap class
+  //HardwareMap hMap = new HardwareMap();
+  //Declare all possible objects here and instantiate what is needed for each bot in constructor
+  //Peanut Bot
+  WPI_VictorSPX   driveLeft_peanut;
+  WPI_TalonSRX    driveRight_peanut;
+  //Bag Bot 
+  WPI_VictorSPX   driveLeftFront_bag;	
+  WPI_VictorSPX   driveLeftRear_bag;
+  WPI_VictorSPX   driveRightFront_bag;
+  WPI_VictorSPX   driveRightRear_bag;
+  // 2nd Bot  
+  Spark         driveLeftFront_2nd; //Use Spark for Victor SP as generic PWM object
+  WPI_TalonSRX	driveLeftRear_2nd;
+  WPI_TalonSRX	driveRightFront_2nd;
+  WPI_TalonSRX	driveRightRear_2nd;
+  // general
+  SpeedController leftSpeedGroup; 
+  SpeedController rightSpeedGroup;
+  DifferentialDrive diffDrive;
   
-  // 2nd bot uses victor speed controlers on can bus except left fron is on PWM
-  WPI_VictorSPX   left_rear_2nd 	= new WPI_VictorSPX(hMap.driveMotorLeftRear);
-  WPI_VictorSPX   right_front_2nd	= new WPI_VictorSPX(hMap.driveMotorRightFront);
-  WPI_VictorSPX   right_rear_2nd	= new WPI_VictorSPX(hMap.driveMotorRightRear);
-  
-  SpeedController leftGroup   = new SpeedControllerGroup(left_front_2nd_and_peanut, left_rear_2nd);
-  SpeedController rightGroup = new SpeedControllerGroup(right_front_2nd, right_rear_2nd);
-   
-  DifferentialDrive r_drive = new DifferentialDrive(leftGroup,rightGroup);
   public DriveTrain(OurBots selectedBot)
   {
-    // switch(selectedBot)
-    // {
-    //   case PEANUT:
-    //     leftGroup   = new SpeedControllerGroup(left_front_2nd_and_peanut, left_rear_peanut);
-    //     righttGroup = new SpeedControllerGroup(right_front_peanut, right_rear_peanut);
-    //     break;
-    //   case WM2019_2ND:
-    //     leftGroup   = new SpeedControllerGroup(left_front_2nd_and_peanut, left_rear_2nd);
-    //     righttGroup = new SpeedControllerGroup(right_front_2nd, right_rear_2nd);
-    //     break;
-    //   case WM2019_BAG:
-    //   default:
-    //     leftGroup   = new SpeedControllerGroup(left_front_bag, left_rear_bag);
-    //     righttGroup = new SpeedControllerGroup(right_front_bag, right_rear_bag);
-    //   break;
-    // }
-  //  r_drive = new DifferentialDrive(leftGroup,righttGroup);
-  //  r_drive.setSafetyEnabled(false);
+    switch(selectedBot)
+    {
+      case PEANUT:
+        driveLeft_peanut	= new WPI_VictorSPX(0); //retain bag bot ID
+        driveRight_peanut	= new WPI_TalonSRX(4);  //retain bag bot ID
+        diffDrive = new DifferentialDrive(driveLeft_peanut,driveRight_peanut);
+        break;
+      case WM2019_2ND:
+        driveLeftFront_2nd  = new Spark(0);//Used Spark for Victor SP as generic PWM object
+        driveLeftRear_2nd   = new WPI_TalonSRX(1);
+        driveRightFront_2nd = new WPI_TalonSRX(2);
+        driveRightRear_2nd  = new WPI_TalonSRX(3);
+        leftSpeedGroup = new SpeedControllerGroup(driveLeftFront_2nd, driveLeftRear_2nd);; 
+        rightSpeedGroup = new SpeedControllerGroup(driveRightFront_2nd, driveRightRear_2nd);;
+        diffDrive = new DifferentialDrive(leftSpeedGroup,rightSpeedGroup);
+        break;
+      case WM2019_BAG:
+      default:
+        driveLeftFront_bag	= new WPI_VictorSPX(0);	
+        driveLeftRear_bag 	= new WPI_VictorSPX(1);
+        driveRightFront_bag	= new WPI_VictorSPX(2);
+        driveRightRear_bag	= new WPI_VictorSPX(3);
+        leftSpeedGroup = new SpeedControllerGroup(driveLeftFront_bag, driveLeftRear_bag);; 
+        rightSpeedGroup = new SpeedControllerGroup(driveRightFront_bag, driveRightRear_bag);;
+        diffDrive = new DifferentialDrive(leftSpeedGroup,rightSpeedGroup);
+        break;
+    }
+    diffDrive.setSafetyEnabled(false);
   }
   public void drive(double left, double right)
   {
      if(Math.abs(left)>0.1 || Math.abs(right)>0.1)
      {
-       r_drive.tankDrive(left,right);
+        diffDrive.tankDrive(left,right);
      }
      else
      {
-       r_drive.tankDrive(0,0);
+        diffDrive.tankDrive(0,0);
      }
   }
 }
